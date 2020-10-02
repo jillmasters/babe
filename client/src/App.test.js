04c49263a2044a06e0096765f3303ad62bc559c1
Hamish <chan.hamish@gmail.com>
2020-10-02 14:46:35 +0100

    test: add basic app test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,25 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+
+// test('renders learn react link', () => {
+//   // const { getByText } = render(<App />);
+//   // const linkElement = getByText(/learn react/i);
+//   // expect(linkElement).toBeInTheDocument();
+// });
 import React from 'react';
-import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { render, screen, cleanup } from '@testing-library/react';
+import 'babel-polyfill';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+require('jest-canvas-mock');
+
+afterEach(cleanup);
+
+test('Dashboard', () => {
+  render(<App />);
+
+  const el = screen.getAllByText('Babe');
+  expect(el[0].textContent).toBeTruthy(); // checks if it exists
+  // expect(el[0].textContent).toBeInTheDocument(); // checks if it exists
 });
