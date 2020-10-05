7f845c666fdc680f91288b25413aa219619681f3
pezza3452 <pezza3452@hotmail.co.uk>
2020-10-05 14:22:19 +0100

    sign up: added unable to sign in test

--- client/src/__tests__/signUp.test.js
+++ client/src/__tests__/signUp.test.js
@@ -4,24 +4,35 @@ import userEvent from '@testing-library/user-event'
 import { navigate } from '@reach/router';
 import '@testing-library/jest-dom/extend-expect'
 import SignUp from '../pages/SignUp';
+import {signup} from '../services/UserService'
 
-import { signup } from '../services/UserService'
-
+import UserService from '../services/UserService'
 jest.mock('../services/UserService');
+
+import authentication from '../authentication';
 jest.mock('../authentication');
-signup.mockResolvedValue('upload');
+signup.mockResolvedValue('');
+
 jest.mock('@reach/router')
 navigate.mockResolvedValue('');
 
+authentication.login.mockResolvedValue('Logged in')
+
+const setIsAuthenticatedMock = jest.fn(() => {});
+const setIsLoadingMock = jest.fn(() => {});
+
 describe('Sign Up', () => {
   afterEach(() => {
+    localStorage.clear()
     jest.clearAllMocks();
   })
 
   it('Correctly filled out form', async () => {
+    // UserService.login.mockResolvedValue({ accessToken: 'TOKEN'});
+
     render(<SignUp
-      setTransactions={val => { console.log('transaction', val) }}
-      setIsLoading={val => { console.log('isloading', val) }} />)
+      setIsAuthenticated={setIsAuthenticatedMock}
+      setIsLoading={setIsLoadingMock} />)
 
     const email = screen.getByLabelText('email');
     expect(email).toBeInTheDocument();
@@ -62,7 +73,14 @@ describe('Sign Up', () => {
     userEvent.click(button);
 
     await waitFor(() => {
+      // expect(localStorage.setItem).toHaveBeenLastCalledWith(
+      //   'accessToken'
+      // );
       expect(signup).toBeCalledTimes(1);
+      expect(setIsLoadingMock).toBeCalledTimes(1);
+      expect(setIsAuthenticatedMock).toBeCalledTimes(1);
+      expect(authentication.login).toBeCalledTimes(1);
+
       expect(signup).toHaveBeenCalledWith({
         email: '23@test',
         password: '123',
@@ -72,13 +90,68 @@ describe('Sign Up', () => {
         currency: '£'
       })
 
-      expect(email).toHaveValue('');
-      expect(password).toHaveValue('');
-      expect(yourName).toHaveValue('');
-      expect(partnerName).toHaveValue('');
-      expect(partnerEmail).toHaveValue('');
+      expect(email.textContent).toEqual('');
+      expect(password.textContent).toEqual('');
+      expect(yourName.textContent).toEqual('');
+      expect(partnerName.textContent).toEqual('');
+      expect(partnerEmail.textContent).toEqual('');
     })
   })
+
+  it('Unable to sign up', async () => {
+    UserService.login.mockImplementation(() => {
+      throw new Error('error');
+    })
+
+    render(<SignUp
+      setIsAuthenticated={setIsAuthenticatedMock}
+      setIsLoading={setIsLoadingMock} />)
+  
+  const email = screen.getByLabelText('email');
+  expect(email).toBeInTheDocument();
+  userEvent.type(email, '23@test');
+  expect(email).toHaveValue('23@test')
+
+  const password = screen.getByLabelText('password');
+  expect(password).toBeInTheDocument();
+  userEvent.type(password, '123');
+  expect(password).toHaveValue('123');
+
+  const gbpCurr = screen.getByLabelText('currency-gbp');
+  expect(gbpCurr).toBeInTheDocument();
+
+  const usdCurr = screen.getByLabelText('currency-usd');
+  expect(usdCurr).toBeInTheDocument();
+
+  userEvent.click(gbpCurr);
+  expect(gbpCurr).toBeChecked();
+
+  const yourName = screen.getByLabelText('your-name');
+  expect(yourName).toBeInTheDocument();
+  userEvent.type(yourName, 'Chris');
+  expect(yourName).toHaveValue('Chris')
+
+  const partnerName = screen.getByLabelText('partners-name');
+  expect(partnerName).toBeInTheDocument();
+  userEvent.type(partnerName, 'Tamara');
+  expect(partnerName).toHaveValue('Tamara')
+
+  const partnerEmail = screen.getByLabelText('partners-email');
+  expect(partnerEmail).toBeInTheDocument();
+  userEvent.type(partnerEmail, 'tamara@test');
+  expect(partnerEmail).toHaveValue('tamara@test')
+
+  const button = screen.getByRole('button');
+  expect(button).toBeInTheDocument();
+  userEvent.click(button);
+
+  await waitFor(() => {
+   
+    expect(email.textContent).toEqual('');
+    expect(password.textContent).toEqual('');
+    expect(yourName.textContent).toEqual('');
+    expect(partnerName.textContent).toEqual('');
+    expect(partnerEmail.textContent).toEqual('');
+  })
 })
-// mock an error??
-// check form gets reset
+})
