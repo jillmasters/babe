f7165ea88656cd7266b996418854805d8ebacaaf
Hamish <chan.hamish@gmail.com>
2020-10-05 16:42:09 +0100

    test: remove cleanups

--- client/src/__tests__/history.test.js
+++ client/src/__tests__/history.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { render, screen, cleanup} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import {navigate } from '@reach/router';
 
 import History from '../pages/History';
 import '@testing-library/jest-dom/extend-expect'
-
-afterEach(cleanup);
 
 jest.mock('@reach/router')
 navigate.mockResolvedValue('');
