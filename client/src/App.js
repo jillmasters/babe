8ff59444870469ac26881f59f757811a5d629b06
Hamish <chan.hamish@gmail.com>
2020-10-05 14:17:14 +0100

    chore: remove commented code

--- client/src/App.js
+++ client/src/App.js
@@ -44,24 +44,6 @@ function App() {
     }
   };
 
-  // const calculateSummary = () => {
-  //   if (transactions) {
-  //     const balance = transactions.reduce((acc, transaction) => {
-  //       if (transaction.lender === users.leadEmail)
-  //         return acc + (transaction.amount * (100 - transaction.split)) / 100;
-  //       else if (transaction.lender === users.partnerEmail)
-  //         return acc - transaction.amount * (transaction.split / 100);
-  //       else {
-  //         const remaining = transaction.amount * transaction.split;
-  //         return acc - remaining;
-  //       }
-  //     }, 0);
-  //     const overallLender = balance > 0 ? users.lead : users.partner;
-  //     const totalOwed = Math.abs(Math.round(balance));
-  //     setSummary({ balance, totalOwed, overallLender });
-  //   }
-  // };
-
   // LOAD DASHBOARD INFO
   useEffect(() => {
     const accessToken = localStorage.getItem('accessToken');
