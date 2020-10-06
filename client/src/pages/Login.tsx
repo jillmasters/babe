da4ec2c96d7cef81bd99bf1e066114028a68e1f6
pezza3452 <pezza3452@hotmail.co.uk>
2020-10-06 14:25:10 +0100

    refactor: Login to TypeScript

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { navigate } from '@reach/router';
+import {
+  MainViewStatic,
+  FormSection,
+  FormLabel,
+  FormInput,
+  FormButton,
+} from '../theme';
+
+import UserService from '../services/UserService';
+import authentication from '../authentication';
+
+const initialState = {
+  email: '',
+  password: '',
+};
+
+interface LoginProps {
+  setIsAuthenticated: Function,
+  setIsLoading: Function,
+}
+
+const Login: React.FC<LoginProps> = ({ setIsAuthenticated, setIsLoading }) => {
+  const [state, setState] = useState(initialState);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setState(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const { email, password } = state;
+    const user = { email, password };
+    try {
+      const result = await UserService.login(user);
+      const { accessToken } = result;
+      localStorage.setItem('accessToken', accessToken);
+      setIsAuthenticated(true);
+      setIsLoading(true);
+      authentication.login(() => navigate('/', { replace: true }));
+    } catch (error) {
+      alert('Your email or password is incorrect. Please try again.');
+      setState(initialState);
+    }
+  };
+
+  return (
+    <MainViewStatic data-testid="login">
+      <h4>
+        <span role="img" aria-label="technologist emoji">
+          🧑‍💻
+        </span>
+        Login
+        <span role="img" aria-label="technologist emoji">
+          🧑‍💻
+        </span>
+      </h4>
+      <form onSubmit={handleSubmit}>
+        <FormSection>
+          <FormLabel htmlFor="email">Email:</FormLabel>
+          <FormInput
+            type="text"
+            name="email"
+            aria-label="email"
+            value={state.email}
+            onChange={handleChange}
+            required
+          />
+        </FormSection>
+        <FormSection>
+          <FormLabel htmlFor="password">Password:</FormLabel>
+          <FormInput
+            type="password"
+            name="password"
+            aria-label="password"
+            value={state.password}
+            onChange={handleChange}
+            required
+          />
+        </FormSection>
+        <FormSection>
+          <FormButton type="submit">Show me the money</FormButton>
+        </FormSection>
+      </form>
+    </MainViewStatic>
+  );
+  };
+
+export default Login;
