6781e7534cbc5d9e00d44f3f4b4d668f23e538b7
pezza3452 <pezza3452@hotmail.co.uk>
2020-10-06 15:18:29 +0100

    refactor: SettleUp to typeScript

---
+++ client/src/pages/SettleUp.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import {
+  MainViewStatic,
+  FormSection,
+  FormInput,
+  FormLabel,
+  FormButton,
+} from '../theme';
+
+const moment = require('moment');
+import TransactionService from '../services/TransactionService';
+import { navigate } from '@reach/router';
+
+import {Transaction, Users, Summary} from '../interfaces'
+
+interface SettleUpProps {
+  summary: Summary,
+  currency: string,
+  users: Users,
+  setTransactions: Function,
+  setIsLoading: Function,
+}
+const SettleUp: React.FC<SettleUpProps> = ({
+  summary,
+  currency,
+  users,
+  setTransactions,
+  setIsLoading,
+}) => {
+  const [note, setNote] = useState('');
+
+  const saveTransaction = (transaction: Transaction) => {
+    TransactionService.postTransaction(transaction)
+      .then(newTransaction =>
+        setTransactions((oldTransactions: Transaction[]) => [
+          ...oldTransactions,
+          newTransaction,
+        ]),
+      )
+      .catch(error => {
+        throw Error('error posting transaction to database');
+      });
+  };
+
+  const submit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newTransaction = {
+      item: `${users.lead} settled up: ${note}`,
+      amount: summary.totalOwed,
+      date: moment(new Date(Date.now())).format('YYYY-MM-DD'),
+      lender: 'Babe',
+      split: -1,
+      addedBy: users.leadEmail,
+    };
+    saveTransaction(newTransaction);
+    setNote('');
+    setIsLoading(true);
+    navigate('/');
+  };
+  return (
+    <MainViewStatic data-testid="settleUp">
+      <h4>
+        <span role="img" aria-label="credit card emoji">
+          💳
+        </span>
+        Settle up
+        <span role="img" aria-label="credit card emoji">
+          💳
+        </span>
+      </h4>
+      <h2>
+        You owe {users.partner} {currency}
+        {summary.totalOwed}.
+        <br />
+        Click to get even.
+      </h2>
+      <form onSubmit={submit}>
+        <FormSection>
+          <FormLabel htmlFor="wipe-description">Leave a note:</FormLabel>
+          <FormInput
+            type="text"
+            name="wipe-description"
+            aria-label="wipe-description"
+            placeholder="💰💰💰"
+            onChange={event => setNote(event.target.value)}
+            value={note}
+          ></FormInput>
+        </FormSection>
+        <FormSection>
+          <FormButton type="submit" aria-label="SettleUp">
+            I&apos;ve paid {users.partner} back
+          </FormButton>
+        </FormSection>
+      </form>
+    </MainViewStatic>
+  );
+};
+
+export default SettleUp;
